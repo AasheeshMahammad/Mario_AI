43a3309f0f638b07c502727dbce8b3768826c914
Aasheesh Mahammad <54064611+AasheeshMahammad@users.noreply.github.com>
2022-01-23 11:37:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from nes_py.wrappers import JoypadSpace
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
 from gym.wrappers import GrayScaleObservation  # reinforcement api
 from stable_baselines3.common.vec_env import vec_frame_stack, dummy_vec_env # Pytorch version of Stable Baselines
-from matplotlib import pyplot as plt
 import os
 from stable_baselines3 import PPO
 from stable_baselines3.common.callbacks import BaseCallback
@@ -13,7 +12,7 @@ LOG_DIR = './logs'
 
 
 class TrainAndLoggingCallback(BaseCallback):
-    def __init__(self,checkFreq,savePath, verbose=1):
+    def __init__(self, checkFreq, savePath, verbose=1):
         super(TrainAndLoggingCallback,self).__init__(verbose)
         self.checkFreq = checkFreq
         self.savePath = savePath
@@ -57,15 +56,27 @@ def preprocess(env):
 
 def learn(env):
     callBack = TrainAndLoggingCallback(checkFreq=10000,savePath=CHECKPOINT_DIR)
-    model =  PPO('CnnPolicy', env, verbose=1, tensorboard_log=LOG_DIR,learning_rate=10**-6,n_steps=512,device='cpu')
-    model.learn(total_timesteps=10*3,callback=callBack)
+    model =  PPO('CnnPolicy', env, verbose=1, tensorboard_log=LOG_DIR, learning_rate=10**-6, n_steps=512)
+    model.learn(total_timesteps=2*10**4, callback=callBack)
     
-
+    
+def test(env):
+    model = PPO.load('./train/best_model_20000')
+    state = env.reset()
+    try:
+        while True:
+            action, state = model.predict(state)
+            state, reward, done, info = env.step(action)
+            env.render()
+    except KeyboardInterrupt:
+        env.close()
 
 def main():
     env = game()
     env = preprocess(env)
     learn(env)
+    test(env)
+
 
 if __name__ == '__main__':
     main()
